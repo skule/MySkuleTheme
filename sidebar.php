08d2456c6e741106e9596d1151cdc02316197ab5
skule <valentinp@gmail.com>
2010-08-20 16:50:25 -0400

    minor fixes

--- sidebar.php
+++ sidebar.php
@@ -43,23 +43,8 @@
 
 <div id="sidebar_right">
 
-<ul>
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar_right') ) : ?>
-
-<li>
-<div class="sidebarbox">
-<h2>Meta</h2>
-<ul>
-  <?php wp_register(); ?>
-  <li><?php wp_loginout(); ?></li>
-  <li><a href="http://validator.w3.org/check/referer" title="This page validates as XHTML 1.0 Transitional">Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a></li>
-  <?php wp_meta(); ?>
-</ul>
-</div>
-</li>
-
 <?php endif; ?>
-</ul>
 
 </div> <!-- Closes Sidebar_right -->
 
