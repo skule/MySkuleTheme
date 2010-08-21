6c9851ad33320e2c45dfc8f4adfe014e637ebb69
skule <valentinp@gmail.com>
2010-08-21 02:42:26 -0400

    added uservoice

--- footer.php
+++ footer.php
@@ -41,5 +41,29 @@
 
 </div><!-- Closes wrapper -->
 </div> <!-- End Main Wrapper -->
+<script type="text/javascript">
+var uservoiceOptions = {
+  /* required */
+  key: 'skule',
+  host: 'skule.uservoice.com', 
+  forum: '15987',
+  showTab: true,  
+  /* optional */
+  alignment: 'left',
+  background_color:'#f00', 
+  text_color: 'white',
+  hover_color: '#06C',
+  lang: 'en'
+};
+
+function _loadUserVoice() {
+  var s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js");
+  document.getElementsByTagName('head')[0].appendChild(s);
+}
+_loadSuper = window.onload;
+window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
+</script>
 </body>
 </html>
