81be22d7c20742aeaed5111a0435367a13ded479
skule <valentinp@gmail.com>
2010-08-20 22:05:53 -0400

    testing file tree

--- documents.php
+++ documents.php
@@ -32,12 +32,11 @@ include "header_documents.php";
 <script type='text/javascript'>
 $(document).ready( function() {
     $('#fileTree').fileTree({
-		root: '<?php echo $_SERVER['DOCUMENT_ROOT'] . '/engsoc-documents/documents/'; ?>',
-        script: '../../jqueryFileTree.php',
+		script: '../../jqueryFileTree.php',
         expandSpeed: -1,
         collapseSpeed: -1,
       }, function(file) {
-		root_path = '<?php echo $_SERVER['DOCUMENT_ROOT'] . '/engsoc-documents/documents/'; ?>';
+		root_path = '<?php echo $_SERVER['DOCUMENT_ROOT'] . '/engsoc-documents/documents/'; ?>' 	 ;
 		
         alert(file.substr(root_path.length()));
     });
