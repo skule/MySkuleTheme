f50a8ebd916fdbff6b3a7427ade4c48269969604
skule <valentinp@gmail.com>
2010-08-24 23:28:27 -0400

    bug fixes

--- sidebar_pages.php
+++ sidebar_pages.php
@@ -17,9 +17,7 @@ if (is_page( )) {
 
   }
   $children=wp_list_pages( 'echo=0&child_of=' . $page . '&title_li=' );
-  if ($children) {
-    $output = wp_list_pages ('echo=0&child_of=' . $page . '&title_li=<h2>' . $title.'</h2>');
-  }
+
 }
 echo $output;
 ?>
