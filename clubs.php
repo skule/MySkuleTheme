c03919aac4a4421aa3e9c50aed9d6f89d4f1cc9d
skule <valentinp@gmail.com>
2010-08-25 00:59:00 -0400

    initial commit

---
+++ clubs.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Template Name: Clubs
+*/
+get_header(); ?>
+
+<div id="main">
+
+<div id="contentwrapper">
+
+<?php if (have_posts()) : ?>
+<?php while (have_posts()) : the_post(); ?>
+
+<div class="topPost">
+  <h2 class="topTitle"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+  <div class="topContent">
+	<table id='clubtable'>
+		<tr>
+		<?php $temp_query = $wp_query; ?>
+		<!-- Do stuff... -->
+
+		<?php query_posts('category_name=clubs&posts_per_page=100'); ?>
+
+		<?php 
+		$i = 1;
+		while (have_posts()) : the_post(); ?>
+			<?php 
+			if($i % 5 == 0)
+			{	
+				echo "
+				</tr>
+				<tr> ";
+				
+			}
+			?>
+				<td>
+					<div class='clubdiv'>
+						<img src='/testclub.jpg' class='clubthumb' />
+						<a href='#'><h3><?php echo $post->post_title; ?></h3></a>
+					</div>
+				</td>
+			<?php
+			
+			$i++;
+			?>
+		<?php endwhile; ?>
+		<?php $wp_query = $temp_query; ?>
+
+
+	
+	
+	</tr>
+	
+	</table>
+  
+  </div>
+  <div class="cleared"></div>
+  <span class="linkpages"><?php wp_link_pages(); ?></span>
+<div class="cleared"></div>
+</div> <!-- Closes topPost -->
+<!--<br/><small><?php edit_post_link('Edit this entry?','',''); ?></small>-->
+
+<?php endwhile; ?>
+
+<?php else : ?>
+
+<div class="topPost">
+  <h2 class="topTitle"><a href="<?php the_permalink() ?>">Not Found</a></h2>
+  <div class="topContent"><p>Sorry, but you are looking for something that isn't here. You can search again by using <a href="#searchform">this form</a>...</p></div>
+</div> <!-- Closes topPost -->
+
+<?php endif; ?>
+
+</div> <!-- Closes contentwrapper-->
+
+
+<?php include (TEMPLATEPATH . '/sidebar_pages.php'); ?>
+<div class="cleared"></div>
+
+</div><!-- Closes Main -->
+
+
+<?php get_footer(); ?>
